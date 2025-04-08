a7651b07194434c35ad02131d081443a8de2e32b
Nikita Lalwani <nktllwn@gmail.com>
2025-04-07 22:33:40 -0700

    Time: 10208 ms (5%), Space: 225.9 MB (5.04%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,38 @@
+class Solution:
+
+    
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        # whenever subsequence asked we use take/ skip method
+
+        self.n = len(nums)
+        dp = [[-1] * (self.n+1) for _ in range(self.n)]
+        self.nums = nums
+
+        def solve(cur, prev):
+            take = 0
+            skip = 0
+
+            if cur == self.n:
+                return 0
+
+            if dp[cur][prev] != -1: return dp[cur][prev]
+
+            if prev == -1 or self.nums[prev] < self.nums[cur]:
+                take = 1 + solve(cur+1, cur)
+            
+            skip = solve(cur+1, prev)
+            dp[cur][prev] = max(take, skip)
+
+            return dp[cur][prev]
+
+        return(solve(0, -1))
+        
+
+
+# Time: O(N^2) due to memoization table of size N x (N+1)
+# Space: O(N^2) for the DP array + recursion stack up to O(N)
+
+# TC = O(2^N) without memoization
+# SC = O(2^N) without memoization because for each position we have 2 options
