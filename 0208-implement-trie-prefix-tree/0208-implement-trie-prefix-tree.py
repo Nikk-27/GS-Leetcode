acdde53c42e81e36a697d7dc33169cfaf1cafd52
Nikita Lalwani <nktllwn@gmail.com>
2025-05-01 03:21:39 -0400

    Time: 59 ms (32.68%), Space: 35.4 MB (10.82%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.endnode = False
+        
+class Trie:
+    # Prefix Tree (Array)
+
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            i = ord(c) - ord("a")
+            if cur.children[i] == None:
+                cur.children[i] = TrieNode()
+            cur = cur.children[i]
+        cur.endnode = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            i = ord(c) - ord("a")
+            if cur.children[i] == None:
+                return False
+            else:
+                cur = cur.children[i]
+        return cur.endnode        
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            i = ord(c) - ord("a")
+            if cur.children[i] == None:
+                return False
+            cur = cur.children[i]
+        return True
+
+# Time complexity: O(n) for each function call.
+# Space complexity: O(t) Where n is the length of the string and t is the total number of TrieNodes created in the Trie.
+        
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
