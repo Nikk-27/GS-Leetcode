8858e110b8532a9e9a3c9801bbe925aa8c22aad9
Nikita Lalwani <nktllwn@gmail.com>
2025-05-24 16:10:16 -0700

    Time: 7 ms (47.51%), Space: 19 MB (72.2%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,39 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Topological Sort (Kahn's Algorithm)
-        incomingindices = [0] * numCourses
-        adj = [[] for i in range(numCourses)] 
-        for i,j in prerequisites:
-            incomingindices[j] += 1
-            adj[i].append(j)
+        # it is cycle detection question , if cycle then False else True
+        # cycle detection using DFS or BFS
+        # BFS method also called Kahn's algo
+        # Topological cycle is possible then its acyclic graph
+        # Because topological sort only possible for DAG
+        # DAG is directed acyclic graph
 
-        q = deque()
-        for i in range(numCourses):
-            if incomingindices[i] == 0:
-                q.append(i)
-        
-        finish = 0
+        def topocheck(numCourses, adjlist, indegree):
+            q = deque()
+            count = 0
 
-        while q:
-            node = q.popleft()
-            finish += 1
-            for i in adj[node]:
-                incomingindices[i] -= 1
-                if incomingindices[i] == 0:
+            for i in range(numCourses):
+                if indegree[i] == 0:
                     q.append(i)
-        return finish == numCourses
+                    count += 1
+            while q:
+                k = q.popleft()
+                for i in adjlist[k]:
+                    indegree[i] -= 1
+                    if indegree[i] == 0:
+                        q.append(i)
+                        count += 1
+            return count == numCourses
 
 
-# TC = O(V + E)
+        indegree = [0] * numCourses
+        adjlist = [[] for i in range(numCourses)]
+
+        for a,b in prerequisites:
+            # b --> a
+            indegree[a] += 1
+            adjlist[b].append(a)        
+
+        return topocheck(numCourses, adjlist, indegree)
+
+# TC = O(V + E), where V = numCourses, E:edges = len(prerequisites)
 # SC = O(V + E)
